d0ff78b20301506a2a6151d0612f8906502bd72b
manishchandrani <142928384+manishchandrani@users.noreply.github.com>
2023-10-30 15:20:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,17 @@
         form.addEventListener('input', updateValue);
         form.addEventListener('change', updateValue);
     
-        function updateValue () {
-            const bill_total = parseFloat(document.getElementById('bill_total').value) || null;
+       function updateValue (e) {
+            const bill_total = document.getElementById('bill_total').value;
+            if(containsAnyLettersOrCharacters(bill_total)){
+                alert("Please Enter a Valid Bill Amount");
+                e.stopPropogation();
+                return;
+            }
+            const bill_value = parseFloat(bill_total);
             const tip_range = parseFloat(document.getElementById('tip_range').value);
-            const tip_value = (bill_total * tip_range) / 100;
-            const total_amount = bill_total + tip_value;
+            const tip_value = (bill_value * tip_range) / 100;
+            const total_amount = bill_value + tip_value;
             if(bill_total <=0){
                 alert("Please Enter a Valid Bill Amount");
                 document.getElementById('tip_range').value = 0;
@@ -21,6 +27,10 @@
             document.getElementById('tip_percentage').value = tip_range.toFixed(2);
             document.getElementById('tip_value').value = tip_value.toFixed(2);
             document.getElementById('total_amount').value = total_amount.toFixed(2);
+        }
+      
+        function containsAnyLettersOrCharacters(str) {
+            return /[a-zA-Z]/.test(str) || /[+-]/.test(str);
         }
     
     });
